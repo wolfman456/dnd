2b40a3384ff333e9989c55cc9227b03482e46b01
wolfman456 <pshook456@gmail.com>
2024-11-20 21:35:37 -0600

    update to nav bar

--- phase3/src/Home.jsx
+++ phase3/src/Home.jsx
@@ -1,8 +1,12 @@
 function HomeScreen() {
     return (
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dolore et impedit numquam possimus sit ut?
-            Culpa cupiditate dolorum, excepturi laborum molestiae nesciunt nihil qui repudiandae sint, tempore ut
-            veritatis.</p>
+       <h1 style={{
+           textAlign: "center",
+           padding: "10px",
+           fontSize:"50px",
+           paddingTop:"100px",
+
+       }}>Welcome</h1>
     )
 }
 
--- phase3/src/nav/Nav.jsx
+++ phase3/src/nav/Nav.jsx
@@ -1,30 +1,36 @@
-import { Link } from 'react-router-dom';
-import useApiCall from '../api-call/UseApi.js'
+import {Link} from 'react-router-dom';
+
+// import useApiCall from '../api-call/UseApi.js'
 
 function NavBar() {
-    const { data, loading, error } = useApiCall("monsters");
-
-    if (loading) return <div>loading</div>;
-    if (error) return <div>Error: {error.message || "An unknown error occurred"}</div>;
+    // const {data, loading, error} = useApiCall("monsters");
+    //
+    // if (loading) return <div>loading</div>;
+    // if (error) return <div>Error: {error.message || "An unknown error occurred"}</div>;
 
     return (
-        <>
-            <ul className="nav nav-tabs" style={{ backgroundColor: 'black' }}>
-                <li className="nav-item">
-                    <a className="nav-link">
-                        <Link to='/'>Home</Link></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#" onClick={()=>{
-                        console.log("here :" + data.results.length);
-                    }}>ability-scores</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-        </>
-    );
+        <ul className="nav nav-tabs" style={{backgroundColor: 'black'}}>
+            <li className="nav-item">
+                <Link to='/' className="nav-link">Home</Link>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">classes</a>
+            </li>
+            <li>
+                <a className="nav-link" href="#">equipment</a>
+            </li>
+            <li>
+                <a className="nav-link" href="#">magic-items</a>
+            </li>
+            <li>
+                <a className="nav-link" href="#">monsters</a>
+            </li>
+            <li>
+                <a className="nav-link" href="#">spells</a>
+            </li>
+        </ul>
+    )
+
 }
 
 export default NavBar;
